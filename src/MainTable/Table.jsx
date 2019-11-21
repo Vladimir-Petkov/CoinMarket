0c2cfab6ac9c76b770b407c2ee554955e8017fc9
Vladimir-Petkov <57286575+Vladimir-Petkov@users.noreply.github.com>
2019-11-21 23:51:39 +0200

    Add some table rows
    Added table rows with percent and charts

--- src/MainTable/Table.jsx
+++ src/MainTable/Table.jsx
@@ -3,6 +3,7 @@ import { Table } from 'react-bootstrap';
 import './styles.css';
 import postService from '../services/post-service';
 import NumberFormat from 'react-number-format';
+import { Sparklines, SparklinesCurve } from 'react-sparklines';
 
 class MainTable extends React.Component {
     state = {
@@ -14,6 +15,8 @@ class MainTable extends React.Component {
             this.setState({ posts })
         });
     }
+
+
 
     render() {
         const { posts } = this.state;
@@ -27,22 +30,28 @@ class MainTable extends React.Component {
                         <th>Market Cap</th>
                         <th>Circulating Supply</th>
                         <th>24h Volume</th>
-                        <th>Last 7 Days</th>
+                        <th>Change (24h)</th>
+                        <th>Last 24h</th>
                     </tr>
                 </thead>
                 <tbody>
                     {Object.keys(posts).map((i) => {
                         let data = posts[i];
-                        //console.log(data);
+                        console.log(data);
                         return (
                             <tr key={data.id}>
                                 <td>{data.market_data.market_cap_rank}</td>
-                                <td><img src={data.image.thumb} alt=''/>{data.name}</td>
-                                <td><NumberFormat value={data.market_data.current_price.usd} displayType={'text'} thousandSeparator={true} prefix={'$'}/></td>
-                                <td><NumberFormat value={data.market_data.market_cap.usd} displayType={'text'} thousandSeparator={true} prefix={'$'}/></td>
-                                <td><NumberFormat value={data.market_data.circulating_supply} displayType={'text'} decimalScale={0} thousandSeparator={true}/></td>
-                                <td><NumberFormat value={data.market_data.total_volume.usd}displayType={'text'} thousandSeparator={true} prefix={'$'}/></td>
-                                <td><NumberFormat value={data.market_data.total_volume.usd}displayType={'text'} thousandSeparator={true} prefix={'$'}/></td>
+                                <td width="170"><img src={data.image.thumb} alt='' />{data.name}</td>
+                                <td className='CriptoName'><NumberFormat value={data.market_data.current_price.usd} displayType={'text'} thousandSeparator={true} prefix={'$'} /></td>
+                                <td><NumberFormat value={data.market_data.market_cap.usd} displayType={'text'} thousandSeparator={true} prefix={'$'} /></td>
+                                <td><NumberFormat value={data.market_data.circulating_supply} displayType={'text'} decimalScale={0} thousandSeparator={true} /></td>
+                                <td className='CriptoName'><NumberFormat value={data.market_data.total_volume.usd} displayType={'text'} thousandSeparator={true} prefix={'$'} /></td>
+                                <td>{data.market_data.market_cap_change_percentage_24h.toFixed(2)}%</td>
+                                <td>
+                                    <Sparklines data={[+data.market_data.high_24h.usd, +data.market_data.low_24h.usd]} width={164} height={48}>
+                                        <SparklinesCurve style={{ stroke: "orange", strokeWidth: "1", fill: "none" }} />
+                                    </Sparklines>
+                                </td>
                             </tr>
                         );
                     })}
