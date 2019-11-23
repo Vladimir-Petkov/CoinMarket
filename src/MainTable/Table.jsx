d17d339d0e7f88d0c95bb5708aeef47e5ab8c9d3
Vladimir-Petkov <57286575+Vladimir-Petkov@users.noreply.github.com>
2019-11-23 10:39:50 +0200

    Color on procents

--- src/MainTable/Table.jsx
+++ src/MainTable/Table.jsx
@@ -4,6 +4,9 @@ import './styles.css';
 import postService from '../services/post-service';
 import NumberFormat from 'react-number-format';
 import { Sparklines, SparklinesCurve } from 'react-sparklines';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+let data = [];
 
 class MainTable extends React.Component {
     state = {
@@ -24,20 +27,21 @@ class MainTable extends React.Component {
             <Table className='table' hover>
                 <thead>
                     <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Price</th>
-                        <th>Market Cap</th>
-                        <th>Circulating Supply</th>
-                        <th>24h Volume</th>
-                        <th>Change (24h)</th>
-                        <th>Last 24h</th>
+                        <th><a href='#'>#</a></th>
+                        <th><a href='#Name'>Name</a></th>
+                        <th><a href='#Price'>Price</a></th>
+                        <th><a href='#Market_Cap'>Market Cap</a></th>
+                        <th><a href='#Circulating_Supply'>Circulating Supply</a></th>
+                        <th><a href='#24h_Volume'>24h Volume</a></th>
+                        <th><a href='#Change_(24h)'>Change (24h)</a></th>
+                        <th><a href='#Last_24h'>Last 24h</a></th>
                     </tr>
                 </thead>
                 <tbody>
                     {Object.keys(posts).map((i) => {
-                        let data = posts[i];
-                        console.log(data);
+                        data = posts[i];
+                        //console.log(data);
+                        let color = data.market_data.market_cap_change_percentage_24h.toFixed(2) > 0 ?'green' : 'red';
                         return (
                             <tr key={data.id}>
                                 <td>{data.market_data.market_cap_rank}</td>
@@ -46,7 +50,7 @@ class MainTable extends React.Component {
                                 <td><NumberFormat value={data.market_data.market_cap.usd} displayType={'text'} thousandSeparator={true} prefix={'$'} /></td>
                                 <td><NumberFormat value={data.market_data.circulating_supply} displayType={'text'} decimalScale={0} thousandSeparator={true} /></td>
                                 <td className='CriptoName'><NumberFormat value={data.market_data.total_volume.usd} displayType={'text'} thousandSeparator={true} prefix={'$'} /></td>
-                                <td>{data.market_data.market_cap_change_percentage_24h.toFixed(2)}%</td>
+                                <td className={color}>{data.market_data.market_cap_change_percentage_24h.toFixed(2)}%</td>
                                 <td>
                                     <Sparklines data={[+data.market_data.high_24h.usd, +data.market_data.low_24h.usd]} width={164} height={48}>
                                         <SparklinesCurve style={{ stroke: "orange", strokeWidth: "1", fill: "none" }} />
